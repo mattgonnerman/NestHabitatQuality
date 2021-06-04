9c4db8282ea9703b4f7c847ea3e6d97ef36c6ff2
Matt Gonnerman <matthew.gonnerman@maine.edu>
2021-06-03 20:35:42 -0400

    consistency changes

--- NestHabitatQuality - 2 Execute JAGS.R
+++ NestHabitatQuality - 2 Execute JAGS.R
@@ -2,87 +2,85 @@ require(R2jags)
 
 ### Data needed for MCMC
 dat <- list(
-  # ### PreLaying Selection ###
-  # yPL = yPL,
-  # wtPL = weightsPL,
-  # cov_PLSel = cov_PLSel,
-  # Ind_PLSel = Ind_PLSel,
-  # NInd_PLSel = NInd_PLSel,
-  # N_PLSel = N_PLSel,
-  # 
-  # ### Laying Selection ###
-  # yL = yL,
-  # wtL = weightsL,
-  # cov_LSel = cov_LSel,
-  # Ind_LSel = Ind_LSel,
-  # NInd_LSel = NInd_LSel,
-  # N_LSel = N_LSel,
+  ### PreLaying Selection ###
+  y_PL = yPL, # Used/Available Specifications
+  wt_PL = weightsPL, #Weights for IWLR
+  cov_PLSel = cov_PLSel, # Spatial Covariates (3Dim Array)
+  NestID_PL = Ind_PLSel, # Numeric Nest ID
+  NGrp_PLSel = NInd_PLSel, # Count of Used/Available Groups
+  NNest_PLSel = N_PLSel, # Count of Individual Nests
+
+  ### Laying Selection ###
+  y_L = yL, # Used/Available Specifications
+  wt_L = weightsL, #Weights for IWLR
+  cov_LSel = cov_LSel, # Spatial Covariates (3Dim Array)
+  NestID_L = Ind_LSel, # Numeric Nest ID
+  NGrp_LSel = NInd_LSel, # Count of Used/Available Groups
+  NNest_LSel = N_LSel, # Count of Individual Nests
 
   ### Nesting Selection ###
-  yN = yN,
-  wtN = weightsN,
-  nNLocs = nNLocs,
-  # cov_N_F = cov_N_F,
-  # cov_N_L = cov_N_L,
-  cov_NSel = as.array(cov_NSel),
-  NInd_NSel = NInd_NSel
+  y_N = yN, # Used/Available Specifications
+  wt_N = weightsN, #Weights for IWLR
+  cov_NSel = cov_NSel, # Spatial Covariates (3Dim Array)
+  NestID_N = Ind_NSel, # Numeric Nest ID
+  NGrp_NSel = NInd_NSel, # Count of Used/Available Groups
+  NNest_NSel = N_NSel, # Count of Individual Nests
   
-  # ### Nest Success ###
-  # NDSR_succ = ns_succ.mat,
-  # NDSR_interval = ns_interval,
-  # NDSR_nvisit = length(ns_succ),
-  # cov_NDSR = cov_NDSR,
-  # NDSR_ID = ns_ID,
-  # 
-  # ### Nesting Habitat Quality Metric ###
-  # cov_NHQ = cov_NHQ,
-  # nNHQ = nrow(cov_NHQ)
+  ### Nest Success ###
+  NDSR_succ = ns_succ.mat,
+  NDSR_interval = ns_interval,
+  NDSR_nvisit = length(ns_succ),
+  cov_NDSR = cov_NDSR,
+  NDSR_ID = ns_ID,
+
+  ### Nesting Habitat Quality Metric ###
+  cov_NHQ = cov_NHQ,
+  nNHQ = nrow(cov_NHQ)
 )
 
 
 ### Parameters monitors
 par.monitor <- c(
-  # ### PreLaying Selection ###
-  # "intercept_PLSel",
-  # "beta_SC_PLSel",
-  # "sigma_PLSel",
-  # "scale_PLSel",
-  # "w_PLSel",
-  # 
-  # ### Laying Selection ###
-  # "intercept_LSel",
-  # "beta_SC_LSel",
-  # "sigma_LSel",
-  # "scale_LSel",
-  # "w_LSel",
+  ### PreLaying Selection ###
+  "intercept_PLSel",
+  "beta_SC_PLSel",
+  "sigma_PLSel",
+  "scale_PLSel",
+  "w_PLSel",
+
+  ### Laying Selection ###
+  "intercept_LSel",
+  "beta_SC_LSel",
+  "sigma_LSel",
+  "scale_LSel",
+  "w_LSel",
 
   ### Nest Selection ###
   "intercept_NSel",
   "beta_SC_NSel",
   "sigma_NSel",
   "scale_NSel",
-  "w_NSel"
+  "w_NSel",
   
-  # ### Nest Success ###
-  # "intercept_NDSR",
-  # "beta_SC_NDSR",
-  # "sigma_NDSR",
-  # "intercept_HDSR",
-  # "beta_SC_HDSR",
-  # "sigma_HDSR",
-  # "intercept_FDSR",
-  # "beta_SC_FDSR",
-  # "sigma_FDSR",
-  # "scale_NDSR",
-  # "w_NDSR",
-  # "scale_HDSR",
-  # "w_HDSR",
-  # "scale_FDSR",
-  # "w_FDSR",
-  # "mean_succ",
-  # 
-  # ### Nesting Habitat Quality Metric ###
-  # "NHQ"
+  ### Nest Success ###
+  "intercept_NDSR",
+  "beta_SC_NDSR",
+  "sigma_NDSR",
+  "intercept_HDSR",
+  "beta_SC_HDSR",
+  "sigma_HDSR",
+  "intercept_FDSR",
+  "beta_SC_FDSR",
+  "sigma_FDSR",
+  "scale_NDSR",
+  "w_NDSR",
+  "scale_HDSR",
+  "w_HDSR",
+  "scale_FDSR",
+  "w_FDSR",
+
+  ### Nesting Habitat Quality Metric ###
+  "NHQ"
 )
 
 
--- NestHabitatQuality - 3 JAGS Model.R
+++ NestHabitatQuality - 3 JAGS Model.R
@@ -39,9 +39,9 @@ function(){#####################################################################
   
   ## Likelihood
   for(i in 1:NGrp_PLSel){
-    yPL[i, 1:11] ~ dmulti(p_PL[i,1:11], 1)
+    y_PL[i, 1:11] ~ dmulti(p_PL[i,1:11], 1)
     for(j in 1:11){
-      p_PL[i,j] <- e_PL[i,j]/sum(wtPL[i,1:11]*e_PL[i,1:11])
+      p_PL[i,j] <- e_PL[i,j]/sum(wt_PL[i,1:11]*e_PL[i,1:11])
       log(e_PL[i,j]) <- alpha[NestID_PL[i]] + beta_SC_PLSel*cov_PLSel[j,scale_PLSel,i] + err_PL[i,j]
     }
   }
@@ -49,115 +49,156 @@ function(){#####################################################################
   #############################################################################
   
   ### Laying Habitat Selection
-  for(i in 1:NInd_LSel){
-    alpha_LSel[i] ~ dbeta(1,1)
-    intercept_LSel[i] <- logit(alpha_LSel[i])
-  }
-  beta_SC_LSel ~ dnorm(0, tau_LSel)
+  ## Priors
+  # Habitat Coefficient
+  beta_SC_LSel ~ dnorm(0, tau_LSel) 
   tau_LSel <- 1/(pow(sigma_LSel,2))
   sigma_LSel ~ dunif(0,50)
+  
+  # BLISS 
   scale_LSel ~ dcat(w_LSel[1:4])
   w_LSel[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
-
   
-  for(i in 1:NInd_LSel){
-    yL[i, 1:nLLocs[i]] ~ dmulti(p_L[i,1:nLLocs[i]], 1)
-    for(j in 1:nLLocs[i]){
-      p_L[i,j] <- e_L[i,j]/sum(wtL[i,1:nLLocs[i]]*e_L[i,1:nLLocs[i]])
-      log(e_L[i,j]) <- intercept_LSel + beta_SC_LSel*cov_LSel[j,scale_LSel,i]
+  # Individual Random Effect
+  alpha_LSel[i:NInd_L] ~ dmnorm(mu_L[1:NInd_L], omega_L[1:NInd_L,1:NInd_L])
+  for(i in 1:NInd_L){
+    mu_L[i] <- 0
+    for(j in 1:NInd_L){
+      omega_L[i,j] ~ dwish(R_L, NInd_L)
+      R_L[i,j] <- 0
     }
   }
-
+  
+  # Random Error 
+  for(i in 1:NNest_LSel){  
+    for(j in 1:11){
+      err_L[i,j] ~ dnorm(0, tau_Lerr) 
+    }
+  }
+  tau_Lerr <- 1/(pow(sigma_Lerr,2))
+  sigma_Lerr ~ dunif(0,50)
+  
+  ## Likelihood
+  for(i in 1:NGrp_LSel){
+    y_L[i, 1:11] ~ dmulti(p_L[i,1:11], 1)
+    for(j in 1:11){
+      p_L[i,j] <- e_L[i,j]/sum(wt_L[i,1:11]*e_L[i,1:11])
+      log(e_L[i,j]) <- alpha[NestID_L[i]] + beta_SC_LSel*cov_LSel[j,scale_LSel,i] + err_L[i,j]
+    }
+  }
+  
+  #############################################################################
   
   ### Nesting Habitat Selection
-  alpha_NSel ~ dbeta(1,1)
-  intercept_NSel <- logit(alpha_NSel)
-  beta_SC_NSel ~ dnorm(0, tau_NSel)
+  ## Priors
+  # Habitat Coefficient
+  beta_SC_NSel ~ dnorm(0, tau_NSel) 
   tau_NSel <- 1/(pow(sigma_NSel,2))
-  sigma_NSel ~ dunif(0,50) 
+  sigma_NSel ~ dunif(0,50)
+  
+  # BLISS 
   scale_NSel ~ dcat(w_NSel[1:4])
   w_NSel[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
-
-
-  for(i in 1:NInd_NSel){
-    yN[i, 1:nNLocs[i]] ~ dmulti(p_N[i,1:nNLocs[i]], 1)
-    for(j in 1:nNLocs[i]){
-      p_N[i,j] <- e_N[i,j]/sum(wtN[i,1:nNLocs[i]]*e_N[i,1:nNLocs[i]])
-      log(e_N[i,j]) <- intercept_NSel + beta_SC_NSel*cov_NSel[j,scale_NSel,i]
+  
+  # Individual Random Effect
+  alpha_NSel[i:NInd_N] ~ dmnorm(mu_N[1:NInd_N], omega_N[1:NInd_N,1:NInd_N])
+  for(i in 1:NInd_N){
+    mu_N[i] <- 0
+    for(j in 1:NInd_N){
+      omega_N[i,j] ~ dwish(R_N, NInd_N)
+      R_N[i,j] <- 0
     }
   }
   
-  # ### Nest/Hen Mort/Flush Daily Failure Risk ###
-  # ## Priors ##
-  # # Nest Only Failure
-  # intercept_NDSR ~ dnorm(0, 0.001)
-  # beta_SC_NDSR ~ dnorm(0, tau_NDSR)
-  # tau_NDSR <- 1/(pow(sigma_NDSR,2))
-  # sigma_NDSR ~ dunif(0,50)  
-  # # Hen Mortality
-  # intercept_HDSR ~ dnorm(0, 0.001)
-  # beta_SC_HDSR ~ dnorm(0, tau_HDSR)
-  # tau_HDSR <- 1/(pow(sigma_HDSR,2))
-  # sigma_HDSR ~ dunif(0,50) 
-  # # Abandoned post flush
-  # intercept_FDSR ~ dnorm(0, 0.001)
-  # beta_SC_FDSR ~ dnorm(0, tau_FDSR)
-  # tau_FDSR <- 1/(pow(sigma_FDSR,2))
-  # sigma_FDSR ~ dunif(0,50)
-  # 
-  # # BLISS
-  # scale_NDSR ~ dcat(w_NDSR[1:4])
-  # w_NDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
-  # scale_HDSR ~ dcat(w_HDSR[1:4])
-  # w_HDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
-  # scale_FDSR ~ dcat(w_FDSR[1:4])
-  # w_FDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
-  # 
-  # #Likelihood
-  # for(n in 1:NDSR_nvisit){
-  #   #Linear Predictors
-  #   cts[n] <- 1
-  #   ctn[n] <- exp(intercept_NDSR + beta_SC_NDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
-  #   cth[n] <- exp(intercept_HDSR + beta_SC_HDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
-  #   ctf[n] <- exp(intercept_FDSR + beta_SC_FDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
-  #   #Denominator
-  #   den[n] <- cts[n] + ctn[n] + cth[n] + ctf[n]
-  #   
-  #   #Daily Survival Probability 
-  #   survp[n] <- cts[n]/den[n]
-  #   
-  #   #Interval Probabilities of Nest/Hen Loss
-  #   p[n,2] <- ((ctn[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n])) 
-  #   p[n,3] <- ((cth[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n])) 
-  #   p[n,4] <- ((ctf[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n]))
-  #   
-  #   #Interval Survival Probability
-  #   p[n,1] <- pow(survp[n], NDSR_interval[n]) 
-  #   
-  #   NDSR_succ[n,1:4] ~ dmulti(p[n,],1)
-  # }
-  # 
-  # # Mean Success Probabilities
-  # mean_succ <- mean(survp)
-  # 
-  ### Habitat Quality ###
-  # P(Selection) x P(Nest Success) x P(Hen Survival) = Quality
-  # for(i in 1:nNHQ){
-  #   # Selection
-  #   PLS[i] <- exp(beta_SC_PLSel*cov_NHQ[i,scale_PLSel])
-  #   LS[i] <- exp(beta_SC_LSel*cov_NHQ[i,scale_LSel])
-  #   NS[i] <- exp(beta_SC_NSel*cov_NHQ[i,scale_NSel])
-  # 
-  #   # Failure Risk
-  #   PN[i] <- exp(intercept_NDSR + beta_SC_NDSR*cov_NHQ[i,scale_NDSR])
-  #   PH[i] <- exp(intercept_HDSR + beta_SC_HDSR*cov_NHQ[i,scale_HDSR])
-  #   PF[i] <- exp(intercept_FDSR + beta_SC_FDSR*cov_NHQ[i,scale_FDSR])
-  #   SuccP[i] <- 1/(1 + PF[i] + PH[i] + PN[i])
-  # 
-  #   #Nesting Habitat Quality Metric
-  #   NHQ[i] <- PLS[i]/max(PLS) * LS[i]/max(LS) * NS[i]/max(NS) * SuccP[i]/max(SuccP)
-  #   # NHQ[i] <- NS[i]/max(NS) * SuccP[i]/max(SuccP)
-  # }
+  # Random Error 
+  for(i in 1:NNest_NSel){  
+    for(j in 1:11){
+      err_N[i,j] ~ dnorm(0, tau_Nerr) 
+    }
+  }
+  tau_Nerr <- 1/(pow(sigma_Nerr,2))
+  sigma_Nerr ~ dunif(0,50)
+  
+  ## Likelihood
+  for(i in 1:NGrp_NSel){
+    y_N[i, 1:11] ~ dmulti(p_N[i,1:11], 1)
+    for(j in 1:11){
+      p_N[i,j] <- e_N[i,j]/sum(wt_N[i,1:11]*e_N[i,1:11])
+      log(e_N[i,j]) <- alpha[NestID_N[i]] + beta_SC_NSel*cov_NSel[j,scale_NSel,i] + err_N[i,j]
+    }
+  }
+  
+  #############################################################################
+  
+  ### Nest/Hen Mort/Flush Daily Failure Risk ###
+  ## Priors ##
+  # Nest Only Failure
+  intercept_NDSR ~ dnorm(0, 0.001)
+  beta_SC_NDSR ~ dnorm(0, tau_NDSR)
+  tau_NDSR <- 1/(pow(sigma_NDSR,2))
+  sigma_NDSR ~ dunif(0,50)
+  
+  # Hen Mortality
+  intercept_HDSR ~ dnorm(0, 0.001)
+  beta_SC_HDSR ~ dnorm(0, tau_HDSR)
+  tau_HDSR <- 1/(pow(sigma_HDSR,2))
+  sigma_HDSR ~ dunif(0,50)
+  
+  # Abandoned post flush
+  intercept_FDSR ~ dnorm(0, 0.001)
+  beta_SC_FDSR ~ dnorm(0, tau_FDSR)
+  tau_FDSR <- 1/(pow(sigma_FDSR,2))
+  sigma_FDSR ~ dunif(0,50)
+
+  # BLISS
+  scale_NDSR ~ dcat(w_NDSR[1:4])
+  w_NDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
+  scale_HDSR ~ dcat(w_HDSR[1:4])
+  w_HDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
+  scale_FDSR ~ dcat(w_FDSR[1:4])
+  w_FDSR[1:4] ~ ddirch(c(0.25,0.25,0.25,0.25))
+
+  #Likelihood
+  for(n in 1:NDSR_nvisit){
+    #Linear Predictors
+    cts[n] <- 1
+    ctn[n] <- exp(intercept_NDSR + beta_SC_NDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
+    cth[n] <- exp(intercept_HDSR + beta_SC_HDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
+    ctf[n] <- exp(intercept_FDSR + beta_SC_FDSR*cov_NDSR[NDSR_ID[n],scale_NDSR])
+    #Denominator
+    den[n] <- cts[n] + ctn[n] + cth[n] + ctf[n]
+
+    #Daily Survival Probability
+    survp[n] <- cts[n]/den[n]
+
+    #Interval Probabilities of Nest/Hen Loss
+    p[n,2] <- ((ctn[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n]))
+    p[n,3] <- ((cth[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n]))
+    p[n,4] <- ((ctf[n]/den[n])/(1 - survp[n]))*(1 - pow(survp[n], NDSR_interval[n]))
+
+    #Interval Survival Probability
+    p[n,1] <- pow(survp[n], NDSR_interval[n])
+
+    NDSR_succ[n,1:4] ~ dmulti(p[n,],1)
+  }
+
+  ## Habitat Quality ###
+  for(i in 1:nNHQ){
+    # Selection
+    PLS[i] <- exp(beta_SC_PLSel*cov_NHQ[i,scale_PLSel])
+    LS[i] <- exp(beta_SC_LSel*cov_NHQ[i,scale_LSel])
+    NS[i] <- exp(beta_SC_NSel*cov_NHQ[i,scale_NSel])
+
+    # Failure Risk
+    PN[i] <- exp(intercept_NDSR + beta_SC_NDSR*cov_NHQ[i,scale_NDSR])
+    PH[i] <- exp(intercept_HDSR + beta_SC_HDSR*cov_NHQ[i,scale_HDSR])
+    PF[i] <- exp(intercept_FDSR + beta_SC_FDSR*cov_NHQ[i,scale_FDSR])
+    SuccP[i] <- 1/(1 + PF[i] + PH[i] + PN[i])
+
+    #Nesting Habitat Quality Metric
+    NHQ[i] <- PLS[i]/max(PLS) * LS[i]/max(LS) * NS[i]/max(NS) * SuccP[i]/max(SuccP)
+    # NHQ[i] <- NS[i]/max(NS) * SuccP[i]/max(SuccP)
+  }
    
   
 }
